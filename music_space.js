766d5767c9f87ddc64174076aa45b1b927401850
Declan Cross <dj3dxx@gmail.com>
2023-09-19 14:43:50 +1200

    Add planet

--- music_space.js
+++ music_space.js
@@ -1,35 +1,35 @@
 // create stars class
 let firstFrame = true;
 const stars = [];
-const maxStarSize = 60;
+const maxStarSize = 30;
 
-class Star{
-  constructor(x, y, size){
+class Star {
+  constructor(x, y, size) {
     this.x = x;
     this.y = y;
     this.size = size;
   }
-  draw(){
+  draw() {
     fill(255, 255, 255, 255);
     ellipse(this.x, this.y, this.size, this.size);
   }
-  drawSized(sizeMod){
+  drawSized(sizeMod, points) {
     fill(255, 255, 255, 255);
-    stroke(0,0,0, 40);
-    star(this.x, this.y, this.size*sizeMod, this.size*sizeMod * 0.3, 4);
+    stroke(0, 0, 0, 40);
+    star(this.x, this.y, this.size * sizeMod, this.size * sizeMod * 0.25, points);
   }
 
-  move(){
-    let shift = this.size/30;
+  move() {
+    let shift = this.size / 60;
     this.x -= shift;
     this.y += shift;
-    if(this.x < 0 || this.y > height){
-      if(random(1) > 0.5){
-        this.x = random(0, width);
+    if (this.x < 0 || this.y > height) {
+      if (random(1) > 0.5) {
+        this.x = random(0, width * 0.9);
         this.y = 0;
-      } else{
+      } else {
         this.x = width;
-        this.y = random(0, height);
+        this.y = random(0, height * 0.9) + height * 0.1;
       }
       this.size = random(1, maxStarSize);
     }
@@ -38,66 +38,156 @@ class Star{
 
 function generateRandomStarSize(maxStarSize) {
   // Adjust the lambda parameter to control the weighting
-  const lambda = 0.4; 
+  const lambda = 0.4;
   const randomValue = Math.random() + 0.1;
   const starSize = -Math.log(1 - randomValue) / lambda;
   return Math.min(starSize, maxStarSize);
 }
 
-function draw_one_frame(words, vocal, drum, bass, other,counter) {
-    background(0, 0, 0, 40);
-    if(firstFrame){
-      for(let i=0; i<30; i++){
-        // generate random size from 1 to maxStarSize but weighted towards smaller stars
+function biasSmallRandom() {
+  return max(0.01, 1 - sqrt(1 - random()));
+}
 
-
-        stars.push(new Star(random(width), random(height), generateRandomStarSize(maxStarSize)));
-      }
-      firstFrame = false;
-    }
-
-
-    // draw stars
-    for(let i=0; i<stars.length; i++){
-      // stars[i].draw();
-      stars[i].drawSized(drum/75);
-      stars[i].move();
-    }
+function draw_one_frame(words, vocal, drum, bass, other, counter) {
+  background(0, 0, 0, 30);
+  // setup items
+  if (firstFrame) {
+    setUp();
+    firstFrame = false;
+  }
 
 
 
-    // circle(width/2, height/2, 400);
+  // draw stars
+  let starSize = drum / 75;
+  let starPoints = 4;
+  if(bass > 70){
+    starSize *= 0.65;
+    // starPoints = random(4, 8);
+  }
+  stars.filter((star) => star.size < maxStarSize * 0.66).forEach((star) => {
+    star.drawSized(starSize, starPoints);
+  });
 
 
 
 
-
-    // draw album cover items
-    textAlign(LEFT);
-    let left = width*0.065;
-    let top = height*0.09;
-    textFont('Lexend');
-    textStyle(BOLD);
-    textSize(width/10);
-    text("SPACE CADET", left, top + width/20);
-    textSize(width/40);
-    let gap = 10;
-    text("METRO BOOMIN", left, top + width/20 + gap + width/40);
-    textAlign(RIGHT);
-    text("GUNNA", width*0.76, top + width/20 + gap + width/40);
-
-    // draw image
-    image(sticker, width - 4*gap - 200, height - 4*gap - 100, 200, 100);
-
-    
+  // draw planet
+  drawPlanet(bass, counter);
 
 
+  stars.filter((star) => star.size >= maxStarSize * 0.66).forEach((star) => {
+    star.drawSized(starSize, starPoints);
+  });
 
-  
+
+  // draw ship and glow
+  drawShip(vocal);
+
+  // draw album cover items
+  drawAlbumCovertItems();
+
+  // move stars
+  stars.forEach((star) => {
+    star.move();
+  });
+
+}
+
+function setUp() {
+  //getnerate stars
+  for (let i = 0; i < 100; i++) {
+    // generate random size from 1 to maxStarSize but weighted towards smaller stars
+    stars.push(new Star(random(width), random(height), biasSmallRandom() * maxStarSize));
+  }
 }
 
 
-// modified from p5.js examples
+function drawPlanet(bass, counter) {
+  noStroke();
+  const planetSize = 120;
+  push();
+  translate(width * 0.2, height * 0.7);
+
+  // bottom layer
+  push();
+    radialGradient(-planetSize / 2, -planetSize / 1.5, 0,
+      -planetSize / 2, -planetSize / 1.5, planetSize * 1.3, color(200), color(0));
+    circle(0, 0, planetSize - (bass/5));
+  pop();
+
+
+  // ring
+  push();
+    rotate(sin(counter/300 - 2)/4);
+    noFill();
+    stroke(200);
+    strokeWeight(9);
+    let bassAmount = bass/5;
+    if(bass > 80){
+      bassAmount += sin(counter)*5;
+    }
+    ellipse(0, 0, planetSize * 1.65 + bassAmount, planetSize * 0.4  + bassAmount/2);
+  pop();
+
+
+  // top layer
+  push();
+    noStroke();
+    fill(255);
+    radialGradient(-planetSize / 2, -planetSize / 1.5, 0,
+    -planetSize / 2, -planetSize / 1.5, planetSize * 1.3, color(200), color(0));
+    arc(0, 0, planetSize - (bass/5), planetSize - (bass/5),  PI, PI *2, CHORD);
+  pop();
+
+
+
+  pop();
+}
+
+
+/**
+ * Draws the ship and its glow
+ * Visualises the vocal value
+ * @param {number} vocalValue - the vocal value
+ */
+function drawShip(vocal) {
+  // draw ship
+
+  // draw ship glow
+  push();
+  radialGradient(width / 2, height / 2, (vocal * 2) + 20, width / 2, height / 2, vocal * 4 + 250, color(0, 0, 0, 0), color(0, 0, 0, 250));
+  // radialGradient(width/2, height/2, (vocalValue*2)+100, width/2, height/2, vocalValue*4, color(0, 0, 0, 0), color(0, 0, 0, 250));
+  rect(0, 0, width, height);
+  pop();
+}
+
+/**
+ * Draws the album cover items
+ * Title and artist names and parental advisory sticker
+ */
+function drawAlbumCovertItems() {
+  // draw album cover items
+  textAlign(LEFT);
+  let left = width * 0.065;
+  let top = height * 0.09;
+  textFont('Lexend');
+  textStyle(BOLD);
+  textSize(width / 10);
+  text("SPACE CADET", left, top + width / 20);
+  textSize(width / 40);
+  let gap = 10;
+  text("METRO BOOMIN", left, top + width / 20 + gap + width / 40);
+  textAlign(RIGHT);
+  text("GUNNA", width * 0.76, top + width / 20 + gap + width / 40);
+
+  // draw image
+  image(sticker, width - 4 * gap - 200, height - 4 * gap - 100, 200, 100);
+}
+
+
+// HELPER FUNCTIONS
+// star modified from p5.js example
 function star(x, y, radius1, radius2, npoints) {
   let angle = TWO_PI / npoints;
   let halfAngle = angle / 2.0;
@@ -112,59 +202,24 @@ function star(x, y, radius1, radius2, npoints) {
   }
   endShape(CLOSE);
 }
+function radialGradient(sX, sY, sR, eX, eY, eR, colorS, colorE) {
+  let gradient = drawingContext.createRadialGradient(
+    sX, sY, sR, eX, eY, eR
+  );
+  gradient.addColorStop(0, colorS);
+  gradient.addColorStop(1, colorE);
+
+  drawingContext.fillStyle = gradient;
+}
 
 
-// background(255,236,180); // cream
-//   fill(244,161,39); // orange
 
-//   let stripeWidth = map(other, 40, 100, 40, 80, true);
-
-//   let numStripes = height / stripeWidth;
-//   for(let i=0; i<numStripes; i=i+2) {
-//     let cury = map(i, 0, numStripes-1, 0, height);
-//     rect(0, cury, width, stripeWidth);
-//   }
-
-//   let triangleHeight = map(bass, 40, 100, 200, 550, true);
-//   fill(117,200,174); // teal
-//   for(let i=0; i<3; i++) {
-//     let cur_x = map(i, 0, 4, 0, width);
-//     let next_x = map(i+1, 0, 3, 0, width);
-//     let mid_x = (cur_x + next_x) / 2.0;
-//     //let cur_y = 4 * height / 5;
-//     let cur_y = height 
-//     triangle(cur_x, cur_y, mid_x, cur_y - triangleHeight, next_x, cur_y);
-//   }
-
-
-//   let drumSize = map(drum, 30, 100, 30, 300, true);
-//   fill(90,61,43); // brown
-//   rect(0, 0, drumSize, drumSize);
-//   rect(width, 0, -drumSize, drumSize);
-//   rect(0, height, drumSize, -drumSize);
-//   rect(width, height, -drumSize, -drumSize);
-
-//   let ovalPlace = map(vocal, 20, 100, height-50, 50, true);
-//   let ovalSize = map(vocal, 20, 100, 60, 150, true);
-//   fill(229,119,30); // darker orange
-//   ellipse(width/2, ovalPlace, ovalSize);
-
-//   if(words == "") {
-//     last_words_opacity = last_words_opacity * 0.95;
-//     words = last_words;
-//   }
-//   else {
-//     last_words_opacity = (1 + last_words_opacity) * 1.1;
-//     if(last_words_opacity > 255) {
-//       last_words_opacity = 255;
-//     }
-//   }
-//   last_words = words;
-
-//   textFont('Georgia');
-//   textAlign(CENTER);
-//   textStyle(BOLD);
-//   textSize(80);
-//   noStroke();
-//   fill(0, 0, 0, int(last_words_opacity));
-//   text(words, width/2, height/2);
+function linearGradient(sX, sY, eX, eY, colorS, colorE) {
+  let gradient = drawingContext.createLinearGradient(
+    sX, sY, eX, eY
+  );
+  gradient.addColorStop(0, colorS);
+  gradient.addColorStop(1, colorE);
+  drawingContext.fillStyle = gradient;
+  // drawingContext.strokeStyle = gradient;
+}
